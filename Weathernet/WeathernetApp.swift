ded418fa1902a402577511358663f47013759b3d
Nursultan- Duysenbaev <2110013@newuzbekistanuniversity.uz>
2023-07-23 14:47:33 +0500

    Initial Commit

---
+++ Weathernet/WeathernetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathernetApp.swift
+//  Weathernet
+//
+//  Created by Nursultan Duysenbaev on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct WeathernetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
